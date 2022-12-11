233cf4f787b2b47eef8bc662c2ef95fba53824ef
Anurag <anurag@git79.com>
2022-12-12 01:02:58 +0530

    SignUp UI Completed

--- app/src/main/java/com/example/theenbhadsah/loginScreen.java
+++ app/src/main/java/com/example/theenbhadsah/loginScreen.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginScreen extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,16 @@ public class loginScreen extends AppCompatActivity {
         setContentView(R.layout.activity_login_screen);
 
         getWindow().setStatusBarColor(ContextCompat.getColor(loginScreen.this,R.color.orange));
+
+        button = findViewById(R.id.signUp);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(loginScreen.this, signUpScreen.class );
+                startActivity(intent);
+            }
+        });
     }
 }
