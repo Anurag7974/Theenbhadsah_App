b1809ecdc8a407f99ae2a30832a87d1155e0302f
Anurag <anurag@git79.com>
2022-12-11 13:40:00 +0530

    Animation code

--- app/src/main/java/com/example/theenbhadsah/splashscreen.java
+++ app/src/main/java/com/example/theenbhadsah/splashscreen.java
@@ -5,13 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class splashscreen extends AppCompatActivity {
+
+    ImageView logo;
+    TextView title, title2;
+    Animation upperanimation, buttom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splashscreen);
+
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+        title2 = findViewById(R.id.title2);
+
+        upperanimation = AnimationUtils.loadAnimation(this, R.anim.upper_animation);
+        buttom = AnimationUtils.loadAnimation(this, R.anim.buttom);
+
+        logo.setAnimation(upperanimation);
+        title.setAnimation(buttom);
+        title2.setAnimation(buttom);
+
     }
 }
